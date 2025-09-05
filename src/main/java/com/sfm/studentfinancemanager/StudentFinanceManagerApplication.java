9a2bd7d8589fb746726725f31f086ee875c23b11
jouharaa <jouhara.abidat01@gmail.com>
2025-09-05 18:07:11 -0400

    chore: initial Spring Boot project

---
+++ src/main/java/com/sfm/studentfinancemanager/StudentFinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sfm.studentfinancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentFinanceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentFinanceManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sfm/studentfinancemanager/StudentFinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfm.studentfinancemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentFinanceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
